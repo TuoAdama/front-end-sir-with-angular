ed5bdfbc12f185ee46b189720b5fa1deeea1748c
Tuo Adama <adama.tuo@etudiant.univ-rennes1.fr>
2023-03-08 20:14:32 +0100

    add: added ticket form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { TicketComponent } from './ticket/ticket.component';
 import { AuthorComponent } from './author/author.component';
 import { TicketItemComponent } from './ticket-item/ticket-item.component';
+import { TicketFormComponent } from './ticket-form/ticket-form.component';
 
 
 const appRoutes: Routes = [
   {path: "", component: DashboardComponent},
   {path: "tickets", component: TicketComponent},
   {path: "authors", component: AuthorComponent},
+  {path: "create-ticket", component: TicketFormComponent},
 ]
 
 @NgModule({
@@ -32,6 +34,7 @@ const appRoutes: Routes = [
     TicketComponent,
     AuthorComponent,
     TicketItemComponent,
+    TicketFormComponent,
   ],
   imports: [
     BrowserModule,

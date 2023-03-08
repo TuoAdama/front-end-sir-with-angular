0bf1d36f338cdc5a6874d104689599e77ce5ee28
Tuo Adama <adama.tuo@etudiant.univ-rennes1.fr>
2023-03-08 15:05:25 +0100

    add: added searchbar component to header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule
